a2b6b66facfdbfebd27b0ff517e0809f723c57e6
Andrew Lou <andy.lou98@gmail.com>
2018-06-10 16:21:03 -0700

    trying to add regex functionality

--- shortener.py
+++ shortener.py
@@ -1,5 +1,14 @@
 from flask import Flask, url_for
+from werkzeug.routing import BaseConverter
+
 app = Flask(__name__)
+
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
 
 @app.route('/')
 def hello_world():
